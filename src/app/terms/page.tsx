c39da126c52867c5970af32c5df8575a854b93e7
psakho922 <psakho922@gmail.com>
2026-02-26 15:03:21 +0000

    Create page.tsx

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,9 @@
+export default function TermsPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">
+        Conditions d'utilisation
+      </h1>
+    </div>
+  );
+}
