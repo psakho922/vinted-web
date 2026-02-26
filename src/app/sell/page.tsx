255605c350525d8054c5d6df9fe0bcd2fb4b75d3
psakho922 <psakho922@gmail.com>
2026-02-26 14:31:21 +0000

    Create page.tsx

---
+++ src/app/sell/page.tsx
@@ -0,0 +1,7 @@
+export default function SellPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Page Sell</h1>
+    </div>
+  );
+}
