08613fd1a8962fd02c7ed62e3a2cf9ef431e21bc
psakho922 <psakho922@gmail.com>
2026-02-26 14:28:35 +0000

    Create page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Page Login</h1>
+    </div>
+  );
+}
