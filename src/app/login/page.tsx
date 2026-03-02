60761fe8025f9b1a220e587f2498ff150b9a43ac
psakho922 <psakho922@gmail.com>
2026-03-02 23:38:39 +0000

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,69 @@
+"use client";
+
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + "/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      const data = await res.json();
+
+      if (res.ok) {
+        localStorage.setItem("token", data.access_token);
+        window.location.href = "/";
+      } else {
+        alert(data.message || "Erreur login");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("Erreur serveur");
+    }
+  }
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div style={{ padding: "40px" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Se connecter</button>
+      </form>
     </div>
   );
 }
