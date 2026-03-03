8e0503de642c8ee3d3f40c4a37e5287da2972572
psakho922 <psakho922@gmail.com>
2026-03-03 00:10:25 +0000

    Update page.tsx

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -1,23 +1,82 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function SellPage() {
-  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
 
-  useEffect(() => {
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
     const token = localStorage.getItem("token");
 
     if (!token) {
-      router.push("/login");
+      alert("Tu dois être connecté");
+      return;
     }
-  }, [router]);
+
+    const userId = JSON.parse(atob(token.split(".")[1])).sub;
+
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_API_URL + "/products",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          price,
+          image,
+          userId,
+        }),
+      }
+    );
+
+    if (res.ok) {
+      alert("Produit créé !");
+      window.location.href = "/";
+    } else {
+      alert("Erreur création produit");
+    }
+  }
 
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Page Sell</h1>
-      <p>Tu es connecté, tu peux vendre un produit.</p>
+    <div style={{ padding: "40px" }}>
+      <h2>Vendre un produit</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Titre"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+
+        <br /><br />
+
+        <input
+          placeholder="Prix"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
+
+        <br /><br />
+
+        <input
+          placeholder="URL image"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          required
+        />
+
+        <br /><br />
+
+        <button type="submit">Créer</button>
+      </form>
     </div>
   );
 }
