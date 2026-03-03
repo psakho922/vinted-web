fe4aa3055d67b4f9117e5b6b3f1fa74535fa45b6
psakho922 <psakho922@gmail.com>
2026-03-03 23:09:42 +0000

    Update page.tsx

--- src/app/product/[ id ]/page.tsx
+++ src/app/product/[ id ]/page.tsx
@@ -1,1 +1,30 @@
+"use client";
 
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+
+export default function ProductPage() {
+  const params = useParams();
+  const id = params.id as string;
+
+  const [product, setProduct] = useState<any>(null);
+
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/products")
+      .then((res) => res.json())
+      .then((data) => {
+        const found = data.find((p: any) => p.id === id);
+        setProduct(found);
+      });
+  }, [id]);
+
+  if (!product) return <p>Chargement...</p>;
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>{product.title}</h1>
+      <p>{product.price} FCFA</p>
+      <img src={product.image} width="400" />
+    </div>
+  );
+}
