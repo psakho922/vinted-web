3267af8427a1eff3104b92890af5fe1bc0fba62a
psakho922 <psakho922@gmail.com>
2026-03-04 01:18:54 +0000

    Create page.tsx

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,27 @@
+ "use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+
+export default function ProductPage() {
+  const params = useParams();
+  const id = params.id as string;
+
+  const [product, setProduct] = useState<any>(null);
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, [id]);
+
+  if (!product) return <p>Chargement...</p>;
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>{product.title}</h1>
+      <p>{product.price} FCFA</p>
+      <img src={product.image} width="400" />
+    </div>
+  );
+}
