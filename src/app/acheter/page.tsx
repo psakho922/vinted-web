f456d48912182664451d30fd9646b4b4533872f1
psakho922 <psakho922@gmail.com>
2026-03-04 22:57:01 +0000

    Create page.tsx

---
+++ src/app/acheter/page.tsx
@@ -0,0 +1,17 @@
+export default function AcheterPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Paiement</h1>
+
+      <p>Le paiement sera disponible bientôt.</p>
+
+      <p>Cette marketplace permettra bientôt de payer avec :</p>
+
+      <ul>
+        <li>Wave</li>
+        <li>Orange Money</li>
+        <li>Carte bancaire</li>
+      </ul>
+    </div>
+  );
+}
