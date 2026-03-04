413651ae776a968c7381b2caea615eab440af806
psakho922 <psakho922@gmail.com>
2026-03-04 22:36:21 +0000

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -17,11 +17,49 @@ export default function ProductPage() {
 
   if (!product) return <p>Chargement...</p>;
 
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>{product.title}</h1>
-      <p>{product.price} FCFA</p>
-      <img src={product.image} width="400" />
-    </div>
-  );
-}
+return (
+  <div style={{ padding: 40 }}>
+    <h1>{product.title}</h1>
+
+    <p>{product.price} FCFA</p>
+
+    <img src={product.image} width="400" />
+
+    <br /><br />
+
+    <button
+      onClick={() => alert("Fonction achat bientôt disponible")}
+      style={{
+        padding: "12px 20px",
+        backgroundColor: "black",
+        color: "white",
+        border: "none",
+        cursor: "pointer"
+      }}
+    >
+      Acheter
+    </button>
+ return (
+  <div style={{ padding: 40 }}>
+    <h1>{product.title}</h1>
+
+    <p>{product.price} FCFA</p>
+
+    <img src={product.image} width="400" />
+
+    <br /><br />
+
+    <button
+      onClick={() => alert("Fonction achat bientôt disponible")}
+      style={{
+        padding: "12px 20px",
+        backgroundColor: "black",
+        color: "white",
+        border: "none",
+        cursor: "pointer"
+      }}
+    >
+      Acheter
+    </button>
+  </div>
+);
