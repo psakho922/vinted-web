2a7fc3a5e5ccb9ca2894a8c55106331e22fa3926
psakho922 <psakho922@gmail.com>
2026-03-05 01:03:32 +0000

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,61 +5,45 @@ import { useParams } from "next/navigation";
 import Link from "next/link";
 
 export default function ProductPage() {
-
   const params = useParams();
   const id = params.id as string;
 
   const [product, setProduct] = useState<any>(null);
 
   useEffect(() => {
+    async function loadProduct() {
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + "/products/" + id
+      );
 
-    fetch(process.env.NEXT_PUBLIC_API_URL + "/products")
-      .then(res => res.json())
-      .then(data => {
+      const data = await res.json();
+      setProduct(data);
+    }
 
-        const found = data.find((p:any) => p.id === id);
-        setProduct(found);
-
-      });
-
+    loadProduct();
   }, [id]);
 
   if (!product) return <p>Chargement...</p>;
 
   return (
-
     <div style={{ padding: 40 }}>
-
       <h1>{product.title}</h1>
 
       <img
         src={product.image}
-        style={{
-          width: 400,
-          marginTop: 20,
-          borderRadius: 10
-        }}
+        style={{ width: 400, marginTop: 20, borderRadius: 10 }}
       />
 
-      <p style={{ fontSize: 22, marginTop: 20 }}>
-        {product.price} FCFA
-      </p>
+      <p style={{ fontSize: 22 }}>{product.price} FCFA</p>
 
       <br />
 
       <Link
         href={"/seller/" + product.userId}
-        style={{
-          color: "blue",
-          textDecoration: "underline",
-          fontSize: 18
-        }}
+        style={{ color: "blue", textDecoration: "underline" }}
       >
         Voir profil vendeur
       </Link>
-
     </div>
-
   );
-
 }
