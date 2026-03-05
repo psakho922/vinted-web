41f69edd101a0d8f86233f3bf2f9cb7cfd7ee8f9
psakho922 <psakho922@gmail.com>
2026-03-05 02:34:12 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,50 +4,29 @@ import Link from "next/link";
 
 export default function Navbar() {
 
-  const token =
-    typeof window !== "undefined"
-      ? localStorage.getItem("token")
-      : null;
-
-  function logout() {
-    localStorage.removeItem("token");
-    window.location.href = "/";
-  }
-
   return (
-    <nav
+
+    <div
       style={{
-        padding: 20,
-        borderBottom: "1px solid #ddd",
         display: "flex",
         gap: 20,
+        padding: 20,
+        borderBottom: "1px solid #ddd"
       }}
     >
 
       <Link href="/">Home</Link>
 
-      {token ? (
-        <>
-          <Link href="/sell">Sell</Link>
+      <Link href="/sell">Sell</Link>
 
-          <Link href="/profile">Profil</Link>
+      <Link href="/panier">Panier 🛒</Link>
 
-          <button onClick={logout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <Link href="/login">Login</Link>
+      <Link href="/login">Login</Link>
 
-          <Link href="/register">Register</Link>
-        </>
-      )}
+      <Link href="/register">Register</Link>
 
-    </nav>
+    </div>
+
   );
+
 }
-
-
-
-
