d317ba716301af311f03b6c9ad04a895cdc32666
psakho922 <psakho922@gmail.com>
2026-03-05 01:55:27 +0000

    Create page.tsx

---
+++ src/app/commande/page.tsx
@@ -0,0 +1,23 @@
+export default function CommandePage() {
+
+  return (
+
+    <div style={{ padding: 40 }}>
+
+      <h1>Commande confirmée 🎉</h1>
+
+      <p>Merci pour votre achat.</p>
+
+      <p>Le vendeur va préparer votre commande.</p>
+
+      <br />
+
+      <a href="/" style={{ color: "blue" }}>
+        Retour à la marketplace
+      </a>
+
+    </div>
+
+  );
+
+}
