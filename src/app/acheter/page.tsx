6bcf81e9cd2d48f230aa82f5d2b5deca91848b40
psakho922 <psakho922@gmail.com>
2026-03-05 01:53:20 +0000

    Update page.tsx

--- src/app/acheter/page.tsx
+++ src/app/acheter/page.tsx
@@ -1,25 +1,65 @@
-export default function AcheterPage(){
+"use client";
 
-  return(
+import { useState } from "react";
 
-    <div style={{padding:40}}>
+export default function AcheterPage() {
+
+  const [method, setMethod] = useState("");
+
+  function payer() {
+
+    if (!method) {
+      alert("Choisissez un moyen de paiement");
+      return;
+    }
+
+    alert("Paiement réussi avec " + method);
+
+    window.location.href = "/commande";
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
 
       <h1>Paiement</h1>
 
-      <p>
-        Le paiement sera disponible bientôt.
-      </p>
+      <p>Choisissez un moyen de paiement :</p>
 
-      <p>Moyens prévus :</p>
+      <br />
 
-      <ul>
-        <li>Wave</li>
-        <li>Orange Money</li>
-        <li>Carte bancaire</li>
-      </ul>
+      <button
+        onClick={() => setMethod("Wave")}
+        style={{
+          padding: 10,
+          marginRight: 10
+        }}
+      >
+        Wave
+      </button>
+
+      <button
+        onClick={() => setMethod("Orange Money")}
+        style={{
+          padding: 10
+        }}
+      >
+        Orange Money
+      </button>
+
+      <br /><br />
+
+      <button
+        onClick={payer}
+        style={{
+          background: "#00a884",
+          color: "white",
+          padding: "10px 20px",
+          borderRadius: 8
+        }}
+      >
+        Payer
+      </button>
 
     </div>
-
   );
-
 }
