b851a058bc9ca5317ab639a9eb6fdb9bb8ff16dd
psakho922 <psakho922@gmail.com>
2026-03-05 01:22:32 +0000

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,45 +5,78 @@ import { useParams } from "next/navigation";
 import Link from "next/link";
 
 export default function ProductPage() {
+
   const params = useParams();
   const id = params.id as string;
 
-  const [product, setProduct] = useState<any>(null);
+  const [product,setProduct] = useState<any>(null);
 
-  useEffect(() => {
-    async function loadProduct() {
-      const res = await fetch(
-        process.env.NEXT_PUBLIC_API_URL + "/products/" + id
-      );
+  useEffect(()=>{
 
-      const data = await res.json();
-      setProduct(data);
-    }
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/products")
+    .then(res=>res.json())
+    .then(data=>{
 
-    loadProduct();
-  }, [id]);
+      const found = data.find((p:any)=>p.id === id);
+      setProduct(found);
 
-  if (!product) return <p>Chargement...</p>;
+    });
 
-  return (
-    <div style={{ padding: 40 }}>
+  },[id]);
+
+  if(!product){
+    return <p>Chargement...</p>;
+  }
+
+  return(
+
+    <div style={{padding:40}}>
+
       <h1>{product.title}</h1>
 
       <img
         src={product.image}
-        style={{ width: 400, marginTop: 20, borderRadius: 10 }}
+        style={{
+          width:400,
+          marginTop:20,
+          borderRadius:10
+        }}
       />
 
-      <p style={{ fontSize: 22 }}>{product.price} FCFA</p>
+      <p style={{fontSize:22}}>
+        {product.price} FCFA
+      </p>
 
-      <br />
+      <br/>
 
       <Link
         href={"/seller/" + product.userId}
-        style={{ color: "blue", textDecoration: "underline" }}
+        style={{
+          color:"blue",
+          textDecoration:"underline"
+        }}
       >
         Voir profil vendeur
       </Link>
+
+      <br/><br/>
+
+      <Link
+        href="/acheter"
+        style={{
+          background:"#00a884",
+          color:"white",
+          padding:"10px 20px",
+          borderRadius:8,
+          textDecoration:"none",
+          fontWeight:"bold"
+        }}
+      >
+        Acheter
+      </Link>
+
     </div>
+
   );
+
 }
