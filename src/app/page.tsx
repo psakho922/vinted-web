afa5e38c48bbbde0c9a4bf7785c774283e526abc
psakho922 <psakho922@gmail.com>
2026-03-10 23:33:25 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,87 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect,useState } from "react";
 import Link from "next/link";
 
-export default function HomePage() {
+export default function HomePage(){
 
-  const [products, setProducts] = useState<any[]>([]);
+  const [products,setProducts] = useState([]);
 
-  useEffect(() => {
+  useEffect(()=>{
 
     fetch(process.env.NEXT_PUBLIC_API_URL + "/products")
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res=>res.json())
+      .then(data=>{
+
         setProducts(data);
+
       });
 
-  }, []);
+  },[]);
 
-  return (
+  return(
 
-    <div style={{ padding: 40 }}>
+    <div>
 
       <h1>Marketplace</h1>
 
-      {products.length === 0 && (
-        <p>Aucun produit pour le moment</p>
-      )}
-
       <div
         style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill,200px)",
-          gap: 20
+
+          display:"grid",
+          gridTemplateColumns:"repeat(auto-fill,minmax(250px,1fr))",
+          gap:"30px",
+          marginTop:"30px"
+
         }}
       >
 
-        {products.map((product) => (
+        {products.map((product:any)=>(
 
           <Link
             key={product.id}
             href={"/product/" + product.id}
+            style={{textDecoration:"none"}}
           >
 
             <div
               style={{
-                border: "1px solid #ddd",
-                padding: 10,
-                cursor: "pointer"
+
+                background:"#fff",
+                padding:"20px",
+                borderRadius:"10px",
+                boxShadow:"0 5px 20px rgba(0,0,0,0.05)",
+                cursor:"pointer"
+
               }}
             >
 
               <img
-                src={
-                  product.image && product.image !== ""
-                    ? product.image
-                    : "https://via.placeholder.com/300"
-                }
-                width="180"
+                src={product.image || "https://via.placeholder.com/300"}
+                width="100%"
                 style={{
-                  objectFit: "cover"
+                  borderRadius:"10px",
+                  height:"200px",
+                  objectFit:"cover"
                 }}
               />
 
-              <h3>{product.title}</h3>
+              <h3 style={{marginTop:"10px"}}>
 
-              <p>{product.price} FCFA</p>
+                {product.title}
+
+              </h3>
+
+              <p
+                style={{
+                  fontWeight:"bold",
+                  color:"#09b1ba"
+                }}
+              >
+
+                {product.price} FCFA
+
+              </p>
 
             </div>
 
