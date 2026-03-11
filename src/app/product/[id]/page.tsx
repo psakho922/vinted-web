31c837371d2f28ec6f6c94a046ca06070aac19ab
psakho922 <psakho922@gmail.com>
2026-03-11 02:50:11 +0000

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,62 +1,30 @@
-"use client";
+<img
+src={product.image}
+width="400"
+/>
 
-import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
-import Link from "next/link";
+<p style={{fontSize:20}}>
+{product.price} FCFA
+</p>
 
-export default function ProductPage() {
+<br/>
 
-  const params = useParams();
-  const id = params.id as string;
+<Link href="/paiement">
 
-  const [product, setProduct] = useState<any>(null);
+<button
+style={{
+padding:12,
+background:"#2563eb",
+color:"white",
+border:"none",
+borderRadius:6,
+cursor:"pointer",
+fontSize:16
+}}
+>
 
-  useEffect(() => {
+Acheter
 
-    fetch(process.env.NEXT_PUBLIC_API_URL + "/products/" + id)
-      .then(res => res.json())
-      .then(data => {
-        setProduct(data);
-      });
+</button>
 
-  }, [id]);
-
-  if (!product) return <p>Chargement...</p>;
-
-  return (
-
-    <div style={{ padding: 40 }}>
-
-      <h1>{product.title}</h1>
-
-      <img
-        src={product.image}
-        width="400"
-        style={{ marginTop: 20, borderRadius: 10 }}
-      />
-
-      <p style={{ fontSize: 20, marginTop: 20 }}>
-        {product.price} FCFA
-      </p>
-
-      <br />
-
-      <Link href="/acheter">
-        <button style={{ padding: 10 }}>
-          Acheter
-        </button>
-      </Link>
-
-      <br /><br />
-
-      <Link href={"/chat/" + product.id}>
-        <button style={{ padding: 10 }}>
-          Poser une question au vendeur
-        </button>
-      </Link>
-
-    </div>
-
-  );
-
-}
+</Link>
