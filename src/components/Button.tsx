ef9f4c9c6b9ff90b2e7fb6e8dcebbea90d4a735b
psakho922 <psakho922@gmail.com>
2026-03-11 00:00:56 +0000

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,19 @@
 "use client";
 
-export default function Button({ children, type = "button", onClick }) {
+import { ReactNode } from "react";
 
+type ButtonProps = {
+  children: ReactNode;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+};
+
+export default function Button({
+  children,
+  type = "button",
+  onClick,
+}: ButtonProps) {
   return (
-
     <button
       type={type}
       onClick={onClick}
@@ -15,14 +25,17 @@ export default function Button({ children, type = "button", onClick }) {
         borderRadius: "8px",
         cursor: "pointer",
         fontWeight: "bold",
-        fontSize: "14px"
+        fontSize: "14px",
+        transition: "0.2s",
+      }}
+      onMouseOver={(e) => {
+        e.currentTarget.style.background = "#078a92";
+      }}
+      onMouseOut={(e) => {
+        e.currentTarget.style.background = "#09b1ba";
       }}
     >
-
       {children}
-
     </button>
-
   );
-
 }
