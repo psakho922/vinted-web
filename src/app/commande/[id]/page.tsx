8fab5843e7c1ab2730480c5dc1365bb1dca2287e
psakho922 <psakho922@gmail.com>
2026-03-11 02:21:11 +0000

    Update page.tsx

--- src/app/commande/[id]/page.tsx
+++ src/app/commande/[id]/page.tsx
@@ -1,21 +1,53 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import { useState } from "react";
 
-export default function CommandePage() {
+export default function CommandePage(){
 
   const params = useParams();
   const id = params.id as string;
 
-  return (
-    <div style={{ padding: 40 }}>
+  const [status,setStatus] = useState("confirmée");
+
+  return(
+
+    <div style={{padding:40}}>
+
       <h1>📦 Suivi de commande</h1>
 
-      <p>ID de la commande :</p>
+      <p>ID de commande :</p>
 
       <h2>{id}</h2>
 
-      <p>Statut : Confirmée</p>
+      <br/>
+
+      <h2>Status : {status}</h2>
+
+      <br/>
+
+      <button
+        onClick={()=>setStatus("préparation")}
+        style={{marginRight:10}}
+      >
+        Préparation
+      </button>
+
+      <button
+        onClick={()=>setStatus("expédiée")}
+        style={{marginRight:10}}
+      >
+        Expédiée
+      </button>
+
+      <button
+        onClick={()=>setStatus("livrée")}
+      >
+        Livrée
+      </button>
+
     </div>
+
   );
+
 }
