726c7ed32cf4b93f63e3dbee0b76c07dd23bc8ec
psakho922 <psakho922@gmail.com>
2026-03-11 02:01:22 +0000

    Update page.tsx

--- src/app/commande/page.tsx
+++ src/app/commande/page.tsx
@@ -1,20 +1,36 @@
-export default function CommandePage() {
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+
+export default function OrderPage() {
+
+  const params = useParams();
+  const id = params.id as string;
+
+  const [order, setOrder] = useState<any>(null);
+
+  useEffect(() => {
+
+    fetch(
+      process.env.NEXT_PUBLIC_API_URL + "/orders/" + id
+    )
+      .then(res => res.json())
+      .then(data => setOrder(data));
+
+  }, [id]);
+
+  if (!order) return <p>Chargement...</p>;
 
   return (
 
     <div style={{ padding: 40 }}>
 
-      <h1>Commande confirmée 🎉</h1>
+      <h1>📦 Suivi de commande</h1>
 
-      <p>Merci pour votre achat.</p>
+      <p>Status :</p>
 
-      <p>Le vendeur va préparer votre commande.</p>
-
-      <br />
-
-      <a href="/" style={{ color: "blue" }}>
-        Retour à la marketplace
-      </a>
+      <h2>{order.status}</h2>
 
     </div>
 
