009d20f73cf681b4460e88ddebc4869d72f0fdf0
psakho922 <psakho922@gmail.com>
2026-04-09 03:18:43 +0000

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,37 +1,95 @@
 "use client";
 
-export default function Login(){
+import { useEffect, useState } from "react";
 
-  const login = () => {
+export default function Profil(){
 
-    const user = {
-      name: "Penda Sakho",
-      email: "penda@gmail.com"
-    };
+  const [user,setUser] = useState<any>(null);
 
-    localStorage.setItem("user", JSON.stringify(user));
+  useEffect(()=>{
+    const u = localStorage.getItem("user");
+    if(u){
+      setUser(JSON.parse(u));
+    }
+  },[]);
 
-    alert("Connexion réussie ✅");
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+    alert("Déconnecté");
   };
 
   return(
 
-    <div style={{padding:"20px"}}>
+    <div style={{
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      minHeight:"80vh"
+    }}>
 
-      <h2>Connexion</h2>
+      {!user ? (
 
-      <button
-        onClick={login}
-        style={{
-          padding:"10px",
-          background:"#09b1ba",
-          color:"#fff",
-          border:"none",
-          borderRadius:"5px"
-        }}
-      >
-        Se connecter
-      </button>
+        <div>
+          <p>Tu n'es pas connecté</p>
+          <a href="/login">
+            <button>Se connecter</button>
+          </a>
+        </div>
+
+      ) : (
+
+        <div
+          style={{
+            background:"#fff",
+            padding:"30px",
+            borderRadius:"20px",
+            boxShadow:"0 15px 40px rgba(0,0,0,0.08)",
+            width:"300px",
+            textAlign:"center"
+          }}
+        >
+
+          {/* 👤 PHOTO */}
+          <img
+            src="https://i.pravatar.cc/150?img=12"
+            style={{
+              width:"100px",
+              height:"100px",
+              borderRadius:"50%",
+              marginBottom:"15px"
+            }}
+          />
+
+          {/* NOM */}
+          <h2 style={{marginBottom:"5px"}}>
+            {user.name}
+          </h2>
+
+          {/* EMAIL */}
+          <p style={{color:"#777", marginBottom:"20px"}}>
+            {user.email}
+          </p>
+
+          {/* BOUTON */}
+          <button
+            onClick={logout}
+            style={{
+              padding:"12px",
+              width:"100%",
+              background:"#ff4d4f",
+              color:"#fff",
+              border:"none",
+              borderRadius:"10px",
+              fontWeight:"bold"
+            }}
+          >
+            Se déconnecter
+          </button>
+
+        </div>
+
+      )}
 
     </div>
 
