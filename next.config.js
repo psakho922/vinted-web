3ea41065837c2061200e6ca4862afc06bb584acb
psakho922 <psakho922@gmail.com>
2026-04-09 02:19:07 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      "res.cloudinary.com",
-      "i.ibb.co",
-      "avatars.githubusercontent.com",
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://api.vinted-web.com",
-    NEXT_PUBLIC_OTHER_VAR: process.env.NEXT_PUBLIC_OTHER_VAR || "",
-  },
-  compiler: {
-    styledComponents: true,
-  },
-  experimental: {
-    appDir: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
