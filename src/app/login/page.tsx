82112ed08b24117a10bafebffb3e11e7080e0b56
psakho922 <psakho922@gmail.com>
2026-04-09 01:51:20 +0000

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,101 +1,30 @@
 "use client";
 
-import { useState } from "react";
+export default function Login(){
 
-export default function LoginPage() {
-
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [message,setMessage] = useState("");
-
-  async function handleLogin(e:any){
-
-    e.preventDefault();
-
-    try{
-
-      const res = await fetch(
-        process.env.NEXT_PUBLIC_API_URL + "/auth/login",
-        {
-          method:"POST",
-          headers:{
-            "Content-Type":"application/json"
-          },
-          body:JSON.stringify({
-            email,
-            password
-          })
-        }
-      );
-
-      const data = await res.json();
-
-      if(!res.ok){
-
-        setMessage("Email ou mot de passe incorrect");
-        return;
-
-      }
-
-      if(typeof window !== "undefined"){
-
-        localStorage.setItem("token",data.access_token);
-
-      }
-
-      setMessage("Login réussi");
-
-      window.location.href="/";
-
-    }catch(err){
-
-      setMessage("Erreur connexion serveur");
-
-    }
-
-  }
+  const login = () => {
+    localStorage.setItem("user","connected");
+    alert("Connexion réussie ✅");
+  };
 
   return(
 
-    <div style={{padding:40}}>
+    <div>
 
-      <h1>Login</h1>
+      <h2>Connexion</h2>
 
-      <form onSubmit={handleLogin}>
-
-        <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e)=>setEmail(e.target.value)}
-        required
-        />
-
-        <br/><br/>
-
-        <input
-        type="password"
-        placeholder="Mot de passe"
-        value={password}
-        onChange={(e)=>setPassword(e.target.value)}
-        required
-        />
-
-        <br/><br/>
-
-        <button type="submit">
-
-          Se connecter
-
-        </button>
-
-      </form>
-
-      {message && (
-
-        <p>{message}</p>
-
-      )}
+      <button
+        onClick={login}
+        style={{
+          padding:"10px",
+          background:"#09b1ba",
+          color:"#fff",
+          border:"none",
+          borderRadius:"5px"
+        }}
+      >
+        Se connecter
+      </button>
 
     </div>
 
