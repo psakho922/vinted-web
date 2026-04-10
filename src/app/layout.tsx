df82f01e442465b7284cd588cc228817330f3064
psakho922 <psakho922@gmail.com>
2026-04-10 02:27:14 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,8 @@ export default function RootLayout({
             <a href="/" style={{marginRight:"15px", color:"#fff"}}>Home</a>
             <a href="/sell" style={{marginRight:"15px", color:"#fff"}}>Sell</a>
             <a href="/panier" style={{marginRight:"15px", color:"#fff"}}>Panier</a>
+            <a href="/commande" style={{marginRight:"15px", color:"#fff"}}>Commande</a>
+            <a href="/chat/1" style={{marginRight:"15px", color:"#fff"}}>Messages</a>
             <a href="/profil" style={{color:"#fff"}}>Profil</a>
           </div>
         </nav>
