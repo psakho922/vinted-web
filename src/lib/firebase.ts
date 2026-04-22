747f44eb554bcd61905b417589c0c828c92683d7
psakho922 <psakho922@gmail.com>
2026-04-22 00:22:59 +0000

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArNQKODtDU3osw1BGX4QsVrRL_VbTmSes",
+  authDomain: "bibani-boutique.firebaseapp.com",
+  projectId: "bibani-boutique",
+  storageBucket: "bibani-boutique.firebasestorage.app",
+  messagingSenderId: "1086032956665",
+  appId: "1:1086032956665:web:329aef28d73828dec46628",
+  measurementId: "G-YM89Z17Y0L"
+};
+
