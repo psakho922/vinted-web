03ea5f75c909cfd0699f10707445f53ffabfd33f
psakho922 <psakho922@gmail.com>
2026-04-22 01:35:14 +0000

    Update page.tsx

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -7,23 +7,6 @@ export default function SellPage(){
   const [title,setTitle] = useState("");
   const [price,setPrice] = useState("");
   const [image,setImage] = useState("");
-  const [name,setName] = useState("");
-  const [phone,setPhone] = useState("");
-
-  const handleImage = (e:any) => {
-
-    const file = e.target.files[0];
-
-    if(file){
-      const reader = new FileReader();
-
-      reader.onloadend = () => {
-        setImage(reader.result as string);
-      };
-
-      reader.readAsDataURL(file);
-    }
-  };
 
   const handleSubmit = () => {
 
@@ -31,9 +14,7 @@ export default function SellPage(){
       id: Date.now(),
       title,
       price,
-      image,
-      sellerName: name,
-      sellerPhone: phone
+      image
     };
 
     let products = JSON.parse(localStorage.getItem("products") || "[]");
@@ -42,59 +23,39 @@ export default function SellPage(){
 
     localStorage.setItem("products", JSON.stringify(products));
 
-    alert("Produit ajouté ✅");
+    alert("Produit ajouté 🔥");
 
     setTitle("");
     setPrice("");
     setImage("");
-    setName("");
-    setPhone("");
   };
 
   return(
 
     <div style={{padding:"20px"}}>
 
-      <h2>Vendre un produit</h2>
+      <h2>Publier un produit</h2>
 
       <input
-        placeholder="Nom du produit"
+        placeholder="Nom produit"
         value={title}
         onChange={(e)=>setTitle(e.target.value)}
-        style={{display:"block", marginBottom:"10px", padding:"10px"}}
+        style={{display:"block", marginBottom:"10px"}}
       />
 
       <input
         placeholder="Prix"
         value={price}
         onChange={(e)=>setPrice(e.target.value)}
-        style={{display:"block", marginBottom:"10px", padding:"10px"}}
+        style={{display:"block", marginBottom:"10px"}}
       />
 
       <input
-        placeholder="Ton nom"
-        value={name}
-        onChange={(e)=>setName(e.target.value)}
-        style={{display:"block", marginBottom:"10px", padding:"10px"}}
+        placeholder="Lien image (optionnel)"
+        value={image}
+        onChange={(e)=>setImage(e.target.value)}
+        style={{display:"block", marginBottom:"10px"}}
       />
-
-      <input
-        placeholder="Numéro téléphone"
-        value={phone}
-        onChange={(e)=>setPhone(e.target.value)}
-        style={{display:"block", marginBottom:"10px", padding:"10px"}}
-      />
-
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImage}
-        style={{marginBottom:"10px"}}
-      />
-
-      {image && (
-        <img src={image} width="200" />
-      )}
 
       <button onClick={handleSubmit}>
         Publier
